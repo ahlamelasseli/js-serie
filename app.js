cb7f51d211db4a7e3dce8f232e2f138e130dfe40
ahlam <ahlamelasseli>
2025-04-10 22:25:48 +0100

    exercise 1

---
+++ app.js
@@ -0,0 +1,11 @@
+
+//~ Exercise 1:
+//  Greetings
+// Create a function called greet that takes a name parameter and prints a greeting message to the console. Call the function with different names.
+
+function greet(name) {
+    console.log("hello " +name+ " how are u");
+    
+}
+greet("bunny")
+
